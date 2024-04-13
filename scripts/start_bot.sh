d6fd10df29c500c675bcfb9c55816c07bfb33281
Duy Phu <duyphu@finhay.com.vn>
2024-04-13 10:48:00 +0700

    add start bot script

---
+++ scripts/start_bot.sh
@@ -0,0 +1,5 @@
+pm2 start python3 --name report_5 -- report_5.py
+pm2 start python3 --name report_28 -- report_28.py
+
+pm2 start python3 --name balance_5 -- balance_5.py
+pm2 start python3 --name balance_28 -- balance_28.py
