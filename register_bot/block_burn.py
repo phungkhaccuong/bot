3a49f8a801a097c30b3fd26f1774f8c7b2a5f24b
phungkhaccuong <cuongst93@gmail.com>
2024-05-12 16:54:13 +0700

    run auto to get target block

---
+++ register_bot/block_burn.py
@@ -0,0 +1,159 @@
+import argparse
+import json
+import time
+
+import bittensor as bt
+from dotenv import load_dotenv
+
+bt.logging.set_debug(False)
+load_dotenv()
+blocks_since_epoch = 0
+old_burn = -1
+path_file_name = 'register_bot/target_block_burn.json'
+
+
+def create_key_in_json_file(netuid):
+    return f"SN_{netuid}"
+
+
+def load_target_block_burn_of_all_subnet():
+    with open(path_file_name, 'r') as json_file:
+        target_block_burn = json.load(json_file)
+    return target_block_burn
+
+
+def save_target_block_burn_of_all_subnet(data):
+    with open(path_file_name, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+def find_target_block_burn(new_blocks_since_epoch):
+    target_block_burn = new_blocks_since_epoch - 2
+    if target_block_burn == -1:
+        target_block_burn = 359
+    elif target_block_burn == -2:
+        target_block_burn = 358
+    return target_block_burn
+
+
+def update_block_burn_of_subnet(netuid, new_blocks_since_epoch):
+    key = create_key_in_json_file(netuid)
+    target_block_burns = load_target_block_burn_of_all_subnet()
+    if key in target_block_burns:
+        target_block_burn = find_target_block_burn(new_blocks_since_epoch)
+        target_block_burns[key] = target_block_burn
+        save_target_block_burn_of_all_subnet(target_block_burns)
+        bt.logging.info(f"update_block_burn_of_subnet of {key} with value {target_block_burn} successful!!!")
+    else:
+        raise Exception(f"Cannot find key {key} in target_block_burns file")
+
+
+def update_target_block_burn(subtensor: bt.subtensor, netuid):
+    global blocks_since_epoch, old_burn
+    subnet = subtensor.get_subnet_info(netuid=netuid)
+    burn = subnet.burn.__float__()
+    new_blocks_since_epoch = subnet.blocks_since_epoch
+
+    if old_burn == -1:
+        old_burn = burn
+
+    if is_ignore_block(netuid, new_blocks_since_epoch):
+        return
+
+    if blocks_since_epoch != new_blocks_since_epoch:
+        bt.logging.info(f"Subnet:{netuid}. Blocks_since_epoch: {new_blocks_since_epoch}. Burn: {burn}")
+        if old_burn != burn:
+            update_block_burn_of_subnet(netuid, new_blocks_since_epoch)
+            old_burn = burn
+
+    blocks_since_epoch = new_blocks_since_epoch
+
+
+def is_ignore_block(netuid, new_blocks_since_epoch):
+    if netuid == 1 and new_blocks_since_epoch == 296:
+        return True
+    if netuid == 2 and new_blocks_since_epoch == 297:
+        return True
+    if netuid == 3 and new_blocks_since_epoch == 298:
+        return True
+    if netuid == 4 and new_blocks_since_epoch == 299:
+        return True
+    if netuid == 5 and new_blocks_since_epoch == 300:
+        return True
+    if netuid == 6 and new_blocks_since_epoch == 301:
+        return True
+    if netuid == 7 and new_blocks_since_epoch == 302:
+        return True
+    if netuid == 8 and new_blocks_since_epoch == 303:
+        return True
+    if netuid == 9 and new_blocks_since_epoch == 304:
+        return True
+    if netuid == 10 and new_blocks_since_epoch == 305:
+        return True
+    if netuid == 11 and new_blocks_since_epoch == 306:
+        return True
+    if netuid == 12 and new_blocks_since_epoch == 307:
+        return True
+    if netuid == 13 and new_blocks_since_epoch == 308:
+        return True
+    if netuid == 14 and new_blocks_since_epoch == 309:
+        return True
+    if netuid == 15 and new_blocks_since_epoch == 310:
+        return True
+    if netuid == 16 and new_blocks_since_epoch == 311:
+        return True
+    if netuid == 17 and new_blocks_since_epoch == 312:
+        return True
+    if netuid == 18 and new_blocks_since_epoch == 313:
+        return True
+    if netuid == 19 and new_blocks_since_epoch == 314:
+        return True
+    if netuid == 20 and new_blocks_since_epoch == 315:
+        return True
+    if netuid == 21 and new_blocks_since_epoch == 316:
+        return True
+    if netuid == 22 and new_blocks_since_epoch == 317:
+        return True
+    if netuid == 23 and new_blocks_since_epoch == 318:
+        return True
+    if netuid == 24 and new_blocks_since_epoch == 319:
+        return True
+    if netuid == 25 and new_blocks_since_epoch == 320:
+        return True
+    if netuid == 26 and new_blocks_since_epoch == 321:
+        return True
+    if netuid == 27 and new_blocks_since_epoch == 322:
+        return True
+    if netuid == 28 and new_blocks_since_epoch == 323:
+        return True
+    if netuid == 29 and new_blocks_since_epoch == 324:
+        return True
+    if netuid == 30 and new_blocks_since_epoch == 325:
+        return True
+    if netuid == 31 and new_blocks_since_epoch == 326:
+        return True
+    if netuid == 32 and new_blocks_since_epoch == 327:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--netuid",
+        type=int,
+        help="netuid",
+        required=True
+    )
+
+    args = parser.parse_args()
+    netuid = args.netuid
+    subtensor = bt.subtensor('finney')
+
+    while True:
+        try:
+            update_target_block_burn(subtensor, netuid)
+            time.sleep(2)
+        except Exception as e:
+            bt.logging.error("[REGISTER] Error: ", e)
+            time.sleep(30)
---
+++ register_bot/block_register.py
@@ -0,0 +1,171 @@
+import argparse
+import json
+import time
+import os
+
+import logging
+
+from register_bot.block_burn import path_file_name, create_key_in_json_file
+
+
+def setup_logger():
+    # Create logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # Create console handler and set level to debug
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Create formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Add formatter to console handler
+    console_handler.setFormatter(formatter)
+
+    # Add console handler to logger
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = setup_logger()
+
+from dotenv import load_dotenv
+from pytest import param
+
+import bittensor as bt
+import requests
+
+bt.logging.set_debug(False)
+load_dotenv()
+
+tele_bot_token = "6408369939:AAHKfnIxyihyXoJ3p1WNN2cgaSD4ielBJtw"
+tele_chat_id = "-4151737386"
+blocks_since_epoch = 0
+
+
+def send_tele(text):
+    data = {
+        "chat_id": tele_chat_id,
+        "text": text,
+        "parse_mode": "HTML"
+    }
+    requests.post(
+        f'https://api.telegram.org/bot{tele_bot_token}/sendMessage',
+        json=data)
+
+
+def direct_register(subtensor: bt.subtensor, wallet: bt.wallet, hotkey: str):
+    subnet = subtensor.get_subnet_info(netuid=netuid)
+    success, err_msg = subtensor._do_burned_register(
+        netuid=netuid,
+        wallet=wallet,
+        wait_for_inclusion=False,
+        wait_for_finalization=True,
+    )
+
+    bt.logging.info(f"[REGISTER] Success: {success}, Error: {err_msg}")
+    if success or 'AlreadyRegistered' in err_msg['name']:
+        send_tele(
+            f'register successfully {netuid} {wallet_name} {hotkey} {subnet.burn}')
+
+
+def get_target_block_burn(netuid):
+    with open(path_file_name, 'r') as json_file:
+        target_block_burn = json.load(json_file)
+
+    key = create_key_in_json_file(netuid)
+    return target_block_burn[key]
+
+
+def register(subtensor: bt.subtensor, wallet: bt.wallet, hotkey: str, wait_seconds: float = 11, max_burn=3,
+             target_block=227):
+    global blocks_since_epoch
+    subnet = subtensor.get_subnet_info(netuid=netuid)
+    burn = subnet.burn.__float__()
+    new_blocks_since_epoch = subnet.blocks_since_epoch
+    # parameters = subtensor.get_subnet_hyperparameters(netuid)
+
+    if blocks_since_epoch != new_blocks_since_epoch:
+        bt.logging.info(f"Blocks_since_epoch: {new_blocks_since_epoch}. Burn: {burn}")
+
+    blocks_since_epoch = new_blocks_since_epoch
+    if blocks_since_epoch == target_block and burn < max_burn:
+        bt.logging.info(f"Block {target_block} reached, waiting for registration: {burn}")
+        time.sleep(wait_seconds)
+        success, err_msg = subtensor._do_burned_register(
+            netuid=netuid,
+            wallet=wallet,
+            wait_for_inclusion=False,
+            wait_for_finalization=True,
+        )
+
+        bt.logging.info(f"[REGISTER] Success: {success}, Error: {err_msg}")
+        if success or 'AlreadyRegistered' in err_msg['name']:
+            send_tele(
+                f'register successfully {netuid} {wallet_name} {hotkey} {subnet.burn}')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--netuid",
+        type=int,
+        help="netuid",
+        required=True
+    )
+    parser.add_argument(
+        "--wallet_name",
+        type=str,
+        help="wallet name",
+        required=True
+    )
+    parser.add_argument(
+        "--hotkey",
+        type=str,
+        help="hotkey",
+        required=True
+    )
+    parser.add_argument(
+        "--max_burn",
+        type=float,
+        help="max burn",
+        required=True
+    )
+    parser.add_argument(
+        "--network",
+        type=str,
+        help="network",
+        default='local'
+    )
+    parser.add_argument(
+        "--wait_seconds",
+        type=float,
+        help="seconds to wait until actual burn",
+        default=10
+    )
+
+    args = parser.parse_args()
+
+    netuid = args.netuid
+    wallet_name = args.wallet_name
+    hotkey = args.hotkey
+    max_burn = args.max_burn
+    network = args.network
+    wait_seconds = args.wait_seconds
+
+    subtensor = bt.subtensor(network=network)
+
+    wallet = bt.wallet(name=wallet_name, hotkey=hotkey)
+    wallet.coldkey
+
+    while True:
+        try:
+            target_block = get_target_block_burn(netuid)
+            bt.logging.info(f"subnet: {netuid}. wait_seconds:{wait_seconds}. target_block: {target_block}")
+            register(subtensor, wallet, hotkey, wait_seconds=wait_seconds, max_burn=max_burn, target_block=227)
+            time.sleep(1)
+        except Exception as e:
+            bt.logging.error("[REGISTER] Error: ", e)
+            time.sleep(30)
